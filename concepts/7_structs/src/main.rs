45d6166a6a740a94a7f12357263c33bf8014bb37
ChihYu Yeh <chihyu.jimmy.yeh@gmail.com>
2022-06-30 08:57:06 +0800

    update

--- concepts/7_structs/src/main.rs
+++ concepts/7_structs/src/main.rs
@@ -19,6 +19,45 @@ struct AlwaysEqual;
 // For structs to store references to data owned by something else would be discussed in lifetimes.
 // Lifetimes ensure that the data referenced by a struct is valid for as long as the struct is.
 
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+// multiple impl blocks is a valid syntax, but no reason to separate these methos into multiple impl blocks here
+impl Rectangle {
+    fn square(size: u32) -> Rectangle {
+        Rectangle {
+            width: size,
+            height: size,
+        }
+    }
+    /*
+    We’ve chosen &self here for the same reason we used &Rectangle in the function version:
+    we don’t want to take ownership, and we just want to read the data in the struct, not write to it.
+    If we wanted to change the instance that we’ve called the method on as part of what the method does, 
+    we’d use &mut self as the first parameter. Having a method that takes ownership of the instance by
+    using just self as the first parameter is rare; this technique is usually used when the method transforms self
+    into something else and you want to prevent the caller from using the original instance after the transformation.
+    */
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+
+    fn width(&self) -> bool {
+        self.width > 0
+    }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+}
+
+fn area(rectangle: &Rectangle) -> u32 {
+    rectangle.width * rectangle.height
+}
+
 fn main() {
     let user1 = User {
         email: String::from("someone@example.com"),
@@ -45,6 +84,37 @@ fn main() {
     println!("({}, {}, {})", black.0, black.1, black.2);
 
     let subject = AlwaysEqual;
+
+    let rect = Rectangle {
+        width: 30,
+        height: 50,
+    };
+    println!(
+        "The area of the rectangle is {} square pixels",
+        area(&rect)
+    );
+    println!("rect: {:?}", rect);
+    println!("rect: {:#?}", rect);
+    println!(
+        "The area of the rectangle is {} square pixels",
+        rect.area()
+    );
+    if rect.width() {
+        println!("The rectangle has a nonzero width; it is {}", rect.width);
+    }
+    let rect2 = Rectangle {
+        width: 10,
+        height: 40,
+    };
+    let rect3 = Rectangle {
+        width: 60,
+        height: 45,
+    };
+    println!("Can rect hold rect2? {}", rect.can_hold(&rect2));
+    println!("Can rect hold rect3? {}", rect.can_hold(&rect3));
+
+    let square = Rectangle::square(3);
+    println!("square: {:#?}", square);
 }
 
 fn build_user(email: String, username: String) -> User {
